837998dfc1819eab94a4f276966d2c3fa3daad7d
gwenbleidd@32 <premiumforges32@gmail.com>
2025-04-17 23:57:38 -0700

    edit

--- migrate.sh
+++ migrate.sh
@@ -19,12 +19,14 @@ BAD_POD=${PODS[$BAD_POD_INDEX]}
 echo "[+] Patching pod $BAD_POD with a special label ..."
 kubectl label pod "$BAD_POD" purpose=parser --overwrite
 
+echo ""
 echo "[✔] Lab setup complete!"
 echo "    All pods are healthy and running."
 echo "    You're only looking for one pod."
 echo ""
 echo "👉 Use this to find it:"
-echo "   The board of team neptune decided to take over control of one e-commerce webserver from the default namespace."
-echo "   The admin who once setup this webserver is not part of the organisation any longer. All information you could get was that the application contains a system was known as 'parser'"
-echo "   Search for the application contiaining the parser system and migrate it into the neptune namespace  "
+echo "   The board of Team Neptune decided to take over control of one e-commerce webserver from the default namespace."
+echo "   The admin who once set up this webserver is no longer part of the organization."
+echo "   All information you could get was that the application contains a system called 'parser'."
+echo "   Search for the application containing the parser system and migrate it into the 'neptune' namespace."
 
